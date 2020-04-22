01e10c901c0e3e2b39d3773d612830ce23622b48
Victor <victorzevallos@protonmail.com>
2020-04-22 16:52:20 -0500

    Add views and urls

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,14 @@
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(
+            PublishedManager, self
+        ).get_queryset().filter(status='published')
 
 
 class Post(models.Model):
@@ -22,8 +30,18 @@ class Post(models.Model):
                             choices=STATUS_CHOICES,
                             default='draft')
 
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
+
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[
+            self.publish.year,
+            self.publish.month,
+            self.publish.day, self.slug
+        ])
