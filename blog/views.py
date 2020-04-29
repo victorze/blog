e1f26f6159d0545c44796cf754c4fb424cef7d5f
Victor <victorzevallos@protonmail.com>
2020-04-29 09:22:15 -0500

    Add custom template tags and filters

--- blog/views.py
+++ blog/views.py
@@ -66,7 +66,7 @@ def post_detail(request, year, month, day, post):
     post_tags_ids = post.tags.values_list('id', flat=True) # [1, 2, 3, ...]
     similar_posts = Post.published.filter(tags__in=post_tags_ids)\
                                     .exclude(id=post.id)
-    # Genera un campo calculado same_ tags
+    # Genera un campo calculado same_tags
     similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
                                     .order_by('-same_tags', '-publish')[:4]
 
